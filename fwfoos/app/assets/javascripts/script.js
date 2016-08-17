1ee705798f9ccfe1e3f6b0eecd8614d48403123d
Oliver Derafera <oliver@futureworkshops.com>
2016-08-17 15:27:40 +0100

    save the code

---
+++ fwfoos/app/assets/javascripts/script.js
@@ -0,0 +1,23 @@
+function modify_qtyblue(val) {
+    var qtyblue = document.getElementById('qtyblue').value;
+    var new_qtyblue = parseInt(qtyblue,10) + val;
+    
+    if (new_qtyblue < 0) {
+        new_qtyblue = 0;
+    }
+    
+    document.getElementById('qtyblue').value = new_qtyblue;
+    return new_qtyblue;
+}
+
+function modify_qtyred(val) {
+    var qtyred = document.getElementById('qtyred').value;
+    var new_qtyred = parseInt(qtyred,10) + val;
+    
+    if (new_qtyred < 0) {
+        new_qtyred = 0;
+    }
+    
+    document.getElementById('qtyred').value = new_qtyred;
+    return new_qtyred;
+}
